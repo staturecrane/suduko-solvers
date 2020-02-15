4d850ba774e8816f6186ea7279b2e210d1fd3d0e
Richard Herbert <richard.alan.herbert@gmail.com>
2020-02-15 11:11:17 -0700

    (haskell) first commit

---
+++ haskell/README.md
@@ -0,0 +1,12 @@
+# Haskell Sudoku Solver
+
+## Install
+
+Make sure you have blas, lapack, and glpk:
+
+
+
+```shell
+sudo apt-get install libblas-dev liblapack-dev
+sudo apt install libglpk-dev
+```
---
+++ haskell/dist-newstyle/build/x86_64-linux/ghc-8.6.5/haskell-0.1.0.0/x/haskell/build/haskell/autogen/Paths_haskell.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_haskell (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/richard/.cabal/bin"
+libdir     = "/home/richard/.cabal/lib/x86_64-linux-ghc-8.6.5/haskell-0.1.0.0-inplace-haskell"
+dynlibdir  = "/home/richard/.cabal/lib/x86_64-linux-ghc-8.6.5"
+datadir    = "/home/richard/.cabal/share/x86_64-linux-ghc-8.6.5/haskell-0.1.0.0"
+libexecdir = "/home/richard/.cabal/libexec/x86_64-linux-ghc-8.6.5/haskell-0.1.0.0"
+sysconfdir = "/home/richard/.cabal/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "haskell_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "haskell_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "haskell_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "haskell_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "haskell_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "haskell_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
