fb3219a378e40fcae08f49b6fb3b9600d751bee3
Richard Herbert <richard.alan.herbert@gmail.com>
2020-02-16 09:27:39 -0700

    (python) solve sudoku with no inputs

--- python/main.py
+++ python/main.py
@@ -35,12 +35,59 @@ def get_row_binaries(solver, grid_char):
 
 def main():
     solver = pywraplp.Solver(
-        "simple_mip_program", pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING
+        "sudoku_solver", pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING
     )
 
     board = []
     for i in range(9):
         board.append(get_row_binaries(solver, i))
+
+    # horizontal contraints
+    # 3 contiguous rows for each set of 3 contiguous grids
+    for i in range(0, 9, 3):
+        grids = board[i : i + 3]
+        # 3 x 9 x 9
+        for j in range(0, 9, 3):
+            row = grids[0][j : j + 3] + grids[1][j : j + 3] + grids[2][j : j + 3]
+            # 9 x 9
+            for k in range(9):
+                solver.Add(
+                    row[0][k]
+                    + row[1][k]
+                    + row[2][k]
+                    + row[3][k]
+                    + row[4][k]
+                    + row[5][k]
+                    + row[6][k]
+                    + row[7][k]
+                    + row[8][k]
+                    == 1
+                )
+
+    # vertical constraints
+    vertical_grid_idxs = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
+    for grid_idxs in vertical_grid_idxs:
+        grids = [board[i] for i in grid_idxs]
+        for i, j, k in vertical_grid_idxs:
+            row = []
+            for l in range(3):
+                row.append(grids[l][i])
+                row.append(grids[l][j])
+                row.append(grids[l][k])
+
+            for m in range(9):
+                solver.Add(
+                    row[0][m]
+                    + row[1][m]
+                    + row[2][m]
+                    + row[3][m]
+                    + row[4][m]
+                    + row[5][m]
+                    + row[6][m]
+                    + row[7][m]
+                    + row[8][m]
+                    == 1
+                )
 
     solver.Maximize(1)
 
@@ -48,11 +95,21 @@ def main():
 
     board_values = []
     for grid in board:
-        row_values = [np.argmax([x.solution_value() for x in row]) + 1 for row in grid]
+        row_values = [
+            np.argmax([x.solution_value() for x in row]).tolist() + 1 for row in grid
+        ]
         board_values.append(row_values)
 
-    print(board_values)
+    return board_values
 
 
 if __name__ == "__main__":
-    main()
+    board_values = main()
+    for i in range(0, 9, 3):
+        grids = board_values[i : i + 3]
+        for j in range(0, 9, 3):
+            one = " ".join([str(x) for x in grids[0][j : j + 3]])
+            two = " ".join([str(x) for x in grids[1][j : j + 3]])
+            three = " ".join([str(x) for x in grids[2][j : j + 3]])
+            print(" | ".join([one, two, three]))
+        print("------ ------- ------")
